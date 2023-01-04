991505a6c6604257b06c16ae1dbd924b4a75bf55
Daniel Melo <daniel.melo42@outlook.com>
2023-01-03 21:07:13 -0700

    DataOrException used by the repository

--- app/src/main/java/com/study/jettrivia/repository/QuestionRepository.kt
+++ app/src/main/java/com/study/jettrivia/repository/QuestionRepository.kt
@@ -1,10 +1,26 @@
 package com.study.jettrivia.repository
 
+import android.util.Log
 import com.study.jettrivia.data.DataOrException
 import com.study.jettrivia.model.QuestionItem
 import com.study.jettrivia.network.QuestionApi
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api: QuestionApi) {
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+    private val dataOrException = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+        } catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d(QuestionRepository::class.java.name, "getAllQuestions: ${exception.localizedMessage}")
+        } finally {
+            dataOrException.loading = false
+        }
+
+        return dataOrException
+    }
+
 }
