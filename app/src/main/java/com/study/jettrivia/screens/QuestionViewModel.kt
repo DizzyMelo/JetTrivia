898514ea36b6481f561cd9dfdb5f56f4e340ab2b
Daniel Melo <daniel.melo42@outlook.com>
2023-01-03 22:10:32 -0700

    made data object public

--- app/src/main/java/com/study/jettrivia/screens/QuestionViewModel.kt
+++ app/src/main/java/com/study/jettrivia/screens/QuestionViewModel.kt
@@ -13,7 +13,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class QuestionViewModel @Inject constructor(private val repository: QuestionRepository): ViewModel() {
-    private val data: MutableState<DataOrException<ArrayList<QuestionItem>, Boolean, Exception>> =
+    val data: MutableState<DataOrException<ArrayList<QuestionItem>, Boolean, Exception>> =
         mutableStateOf(DataOrException(null, false, Exception("")))
 
     init {
